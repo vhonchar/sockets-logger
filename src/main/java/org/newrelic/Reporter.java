673e46afdda36ab58ebe6a18f5baa72d19675d42
Volodymyr_Honchar <vhonchar94@gmail.com>
2023-02-01 14:51:52 -0800

    Reporter to calculate delta in metrics between reports calls

---
+++ src/main/java/org/newrelic/Reporter.java
@@ -0,0 +1,31 @@
+package org.newrelic;
+
+import static java.lang.String.format;
+
+public class Reporter {
+
+    private static final String REPORT_FORMAT = "Received %s unique numbers, %s duplicates. Unique total: %s";
+    private final MetricsService metrics;
+    private int lastUnique = 0;
+    private int lastDuplicates = 0;
+
+    public Reporter(MetricsService metrics) {
+        this.metrics = metrics;
+    }
+
+    public String runReport() {
+        var totalUnique = this.metrics.unique().get();
+        var totalDuplicates = this.metrics.duplicates().get();
+
+        var report = format(REPORT_FORMAT,
+                totalUnique - this.lastUnique,
+                totalDuplicates - this.lastDuplicates,
+                totalUnique
+        );
+
+        this.lastDuplicates = totalDuplicates;
+        this.lastUnique = totalUnique;
+
+        return report;
+    }
+}
---
+++ src/test/java/org/newrelic/ReporterTest.java
@@ -0,0 +1,41 @@
+package org.newrelic;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ReporterTest {
+
+    private Reporter reporter;
+    private MetricsService metrics;
+
+    @BeforeEach
+    public void setup() {
+        this.metrics = new MetricsService();
+        this.reporter = new Reporter(metrics);
+    }
+
+    @Test
+    void shouldReportZeroInitialData() {
+        assertEquals("Received 0 unique numbers, 0 duplicates. Unique total: 0", this.reporter.runReport());
+    }
+
+    @Test
+    void shouldReportTotalOnFirstRun() {
+        this.metrics.unique().getAndAdd(500);
+        this.metrics.duplicates().getAndAdd(1_000_000);
+        assertEquals("Received 500 unique numbers, 1000000 duplicates. Unique total: 500", this.reporter.runReport());
+    }
+
+    @Test
+    void shouldReportDeltaFromLastRun() {
+        this.metrics.unique().getAndAdd(500);
+        this.metrics.duplicates().getAndAdd(1_000_000);
+        assertEquals("Received 500 unique numbers, 1000000 duplicates. Unique total: 500", this.reporter.runReport());
+
+        this.metrics.unique().getAndAdd(300);
+        this.metrics.duplicates().getAndAdd(5000);
+        assertEquals("Received 300 unique numbers, 5000 duplicates. Unique total: 800", this.reporter.runReport());
+    }
+}
